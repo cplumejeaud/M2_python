11e5ad7290df80d230c739294a5d2e4d81ea7511
cplumejeaud <cplumejeaud@gmail.com>
2020-11-12 17:20:23 +0100

    adapt dbscan
    adapt dbscan to split wintering areas into smallest pieces

--- data/arctox/code/STDBSCAN.py
+++ data/arctox/code/STDBSCAN.py
@@ -74,15 +74,32 @@ def retrieve_neighbors(index_center, df, spatial_threshold, temporal_threshold):
 if __name__ == "__main__":
     pass
 
-    csv_path = argv[1]
+    #csv_path = argv[1]
+    # Select points that are in the cluster having the longest duration per bird and where cid is not null
+
+    ## Adapt 
+    query  = """    
+    select c.id, c.cid, c.duration, g.pkid, g.smooth_lat as latitude, g.smooth_long as longitude, g.timestampgps as date_time
+    from 
+	(select c.id, max(duration) as m
+	from arctic.spatial_cluster c 
+	where duration is not null
+	group by c.id) as  k , 
+	arctic.spatial_cluster c,
+	arctic.kap_hoegh_gls g  
+	where c.id = k.id and c.duration = k.m and g.pkid = ANY  (c.pkids_in_cluster) 
+
+    """
+    engine = create_engine('postgresql://maliha:malihaM2@localhost:5432/savoie')
+    df_table = sql.read_sql_query(query, engine)
 
     # df_table must have the columns: 'latitude', 'longitude' and 'date_time'
-    df_table = pd.read_csv(csv_path)
+    #df_table = pd.read_csv(csv_path)
     print(df_table.columns)
     print(df_table.shape)
 
-    spatial_threshold = 100 # meters
-    temporal_threshold = 1  # minutes
-    min_neighbors = 2
+    spatial_threshold = 350000 # meters
+    temporal_threshold = ??? # minutes
+    min_neighbors = 5
     df_clustering = ST_DBSCAN(df_table, spatial_threshold, temporal_threshold, min_neighbors)
     print(df_clustering)
